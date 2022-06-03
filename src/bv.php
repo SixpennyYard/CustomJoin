52b10cd851e7b01f84403bb4b5b9c560ef59f5d0
SixpennyYard <88838512+SixpennyYard@users.noreply.github.com>
2022-06-03 16:36:06 +0200

    Add files via upload

---
+++ src/bv.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace SixpennyYard\CustomJoin;
+
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\plugin\Plugin;
+use SixpennyYard\CustomJoin\Main;
+use pocketmine\player\Player;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+
+class bv extends Command{
+    
+    private $plugin;
+    
+    public function __construct() {
+        
+        parent::__construct("wc", "send a welcome message", "§cusage: /wc <player> or /bv <player>");
+        $this->setPermission('use.wc');
+        
+    }
+    public function execute(CommandSender $sender, string $commandLabel, array $args)    {
+        
+        if(empty($args[0])){
+            $sender->sendMessage("§cusage: /wc player or /bv player");
+            return false;
+                
+        }
+
+        $bvmsg = CustomJoin::getInstance()->getConfig()->get("Welcome-Message");
+        $bvmsg = str_replace("{jplayer}", $args[0], $bvmsg);
+        CustomJoin::getInstance()->getServer()->broadcastMessage($bvmsg);
+        
+    }
+    
+}
